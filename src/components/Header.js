3160e5a9095dd78ad3150765a47d61457c0ea350
Oscar Sánchez <ojsm45@gmail.com>
2019-10-28 22:58:00 -0300

    added header comp

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className='uk-margin'>
+    <h1 className='uk-text-center'>Eventos de React con EventBrite</h1>
+  </header>
+);
+
+export default Header;
