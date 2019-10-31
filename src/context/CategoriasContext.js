2f852f6ccd03792e8f80872ca6e5e6a429edd613
Oscar Sánchez <ojsm45@gmail.com>
2019-10-31 00:06:41 -0300

    before install axios

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// Crear el context
+const CategoriasContext = React.createContext();
+export const CategoriasConsumer = CategoriasContext.Consumer;
+
+export default class CategoriasProvider extends Component {
+  token = 'ME4J66XK73WRIWVKNX';
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
