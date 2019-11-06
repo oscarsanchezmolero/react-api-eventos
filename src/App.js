5faef0f2d9ab5cd8c53164fd0733915b843c54b3
Oscar Sánchez <ojsm45@gmail.com>
2019-11-05 23:24:17 -0300

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 import CategoriasProvider from './context/CategoriasContext';
 
 //
@@ -9,6 +10,9 @@ function App() {
   return (
     <CategoriasProvider>
       <Header />
+      <div className='uk-container'>
+        <Formulario />
+      </div>
     </CategoriasProvider>
   );
 }
